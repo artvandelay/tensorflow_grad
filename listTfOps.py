c179ce339d971c9465a2c5f570d64775f572ce05
jigar <ubuntu@ip-172-31-11-251.ec2.internal>
2017-06-02 08:07:23 +0000

    this lists all the ops

---
+++ listTfOps.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow.core.framework import op_def_pb2
+from google.protobuf import text_format
+    
+def get_op_types(op):
+    for attr in op.attr:
+        if attr.type != 'type':
+            continue
+        return list(attr.allowed_values.list.type)
+    return []
+
+# directory where you did "git clone"
+tensorflow_git_base = "tensorflow/"
+ops_file = tensorflow_git_base+"/tensorflow/core/ops/ops.pbtxt"
+ops = op_def_pb2.OpList()
+text_format.Merge(open(ops_file).read(), ops)
+
+for op in ops.op:
+    # get templated string types
+    if tf.string in get_op_types(op):
+        print op.name, op.summary
+    for arg in op.input_arg:
+        if arg.type == tf.string:
+            print op.name, op.summary
+            break
+        
+
